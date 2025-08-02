057cefa5a1a73858a3a1bad668a65b62b6325398
WANT <490055345@qq.com>
2025-08-02 22:50:20 +0800

    update src file from pakeplus

---
+++ src/config.js
@@ -0,0 +1,12 @@
+window.config = {
+  // 服务地址
+  // BaseUrl: 'http://118.195.214.107:8888/mes-server/',
+  // BaseUrl: 'http://118.195.214.107:8090/qingliang-server',
+  BaseUrl: 'http://58.246.234.210:8888/mes-server',
+  // 页面数据刷新时间(ms),传0关闭刷新
+  refreshDataTime: 20000,
+  // 页面停留时间(ms),传0关闭切换
+  pageStayTime: 0,
+  singleDataTime: 15000,
+  reloadTime: 2400000
+}
